4364a20e9131942d6997bdde53fa7a6019bcffd0
Hari Govind <harigovindsvrindavan@gmail.com>
2022-09-05 12:26:31 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express')
+const { Socket } = require('socket.io')
+var app = express()
+const { PeerServer } = require('peer');
+const { ExpressPeerServer } = require('peer');
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+
+      socket.join(roomId)
+      socket.to(roomId).emit('user-connected', userId)
+
+  
+      socket.on('disconnect', () => {
+        console.log("User disconnected")
+        socket.to(roomId).emit('user-disconnected', userId)
+        
+      })
+
+  })
+
+})
+
+server.listen(3000)
